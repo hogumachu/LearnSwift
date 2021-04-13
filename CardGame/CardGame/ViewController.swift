4795718b93052e9e1a4cc9fb221754650283c298
hogumachu <castle_jun@daum.net>
2021-04-13 23:32:11 +0900

    StackView (Autolayout), TODO : newGameButton Action

--- CardGame/CardGame/ViewController.swift
+++ CardGame/CardGame/ViewController.swift
@@ -13,13 +13,12 @@ class ViewController: UIViewController {
     var cards: [UIButton] = []
     let flipCountLabel = UILabel()
     let newGameButton = UIButton()
-    
-    
-    var flipCount = 0 {
-        didSet {
-            flipCountLabel.text = "Flips: \(flipCount)"
-        }
-    }
+    let firstStackView = UIStackView()
+    let secondStackView = UIStackView()
+    let thirdStackView = UIStackView()
+    let lastStackView = UIStackView()
+    let sumStackView = UIStackView()
+    var flipCount = 0 { didSet { flipCountLabel.text = "Flips: \(flipCount)" } }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,79 +26,9 @@ class ViewController: UIViewController {
     }
     
     func settingView() {
-        for _ in 1...16 {
-            let card = UIButton()
-            card.cardSetting(title: "", backgroundColor: .orange, view: view)
-            cards.append(card)
-        }
-        
-        for card in cards {
-            view.addSubview(card)
-            card.addTarget(self, action: #selector(touchUpCard), for: .touchUpInside)
-        }
-        
-        newGameButton.addTarget(self, action: #selector(touchUpNewGame), for: .touchUpInside)
-        view.addSubview(flipCountLabel)
-        view.backgroundColor = .black
-        
-        view.addSubview(newGameButton)
-        
-        flipCountLabel.text = "Flips: 0"
-        flipCountLabel.textColor = .white
-        flipCountLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        newGameButton.setTitle("New Game", for: .normal)
-        newGameButton.setTitleColor(.white, for: .normal)
-        newGameButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            flipCountLabel.leftAnchor.constraint(equalTo: view.layoutMarginsGuide.leftAnchor, constant: 5),
-            flipCountLabel.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: 5),
-            flipCountLabel.heightAnchor.constraint(equalToConstant: 50),
-            flipCountLabel.widthAnchor.constraint(equalToConstant: 100),
-            
-            newGameButton.rightAnchor.constraint(equalTo: view.layoutMarginsGuide.rightAnchor, constant: 5),
-            newGameButton.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: 5),
-            newGameButton.heightAnchor.constraint(equalTo: flipCountLabel.heightAnchor),
-            newGameButton.widthAnchor.constraint(equalTo: flipCountLabel.widthAnchor),
-            
-            cards[0].leftAnchor.constraint(equalTo: view.layoutMarginsGuide.leftAnchor, constant: 20),
-            cards[0].topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 20),
-            cards[1].leftAnchor.constraint(equalTo: cards[0].rightAnchor, constant: 5),
-            cards[1].topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 20),
-            cards[2].leftAnchor.constraint(equalTo: cards[1].rightAnchor, constant: 5),
-            cards[2].topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 20),
-            cards[3].leftAnchor.constraint(equalTo: cards[2].rightAnchor, constant: 5),
-            cards[3].topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 20),
-            
-            cards[4].leftAnchor.constraint(equalTo: view.layoutMarginsGuide.leftAnchor, constant: 20),
-            cards[4].topAnchor.constraint(equalTo: cards[0].bottomAnchor, constant: 5),
-            cards[5].leftAnchor.constraint(equalTo: cards[4].rightAnchor, constant: 5),
-            cards[5].topAnchor.constraint(equalTo: cards[0].bottomAnchor, constant: 5),
-            cards[6].leftAnchor.constraint(equalTo: cards[5].rightAnchor, constant: 5),
-            cards[6].topAnchor.constraint(equalTo: cards[0].bottomAnchor, constant: 5),
-            cards[7].leftAnchor.constraint(equalTo: cards[6].rightAnchor, constant: 5),
-            cards[7].topAnchor.constraint(equalTo: cards[0].bottomAnchor, constant: 5),
-            
-            cards[8].leftAnchor.constraint(equalTo: view.layoutMarginsGuide.leftAnchor, constant: 20),
-            cards[8].topAnchor.constraint(equalTo: cards[4].bottomAnchor, constant: 5),
-            cards[9].leftAnchor.constraint(equalTo: cards[8].rightAnchor, constant: 5),
-            cards[9].topAnchor.constraint(equalTo: cards[4].bottomAnchor, constant: 5),
-            cards[10].leftAnchor.constraint(equalTo: cards[9].rightAnchor, constant: 5),
-            cards[10].topAnchor.constraint(equalTo: cards[4].bottomAnchor, constant: 5),
-            cards[11].leftAnchor.constraint(equalTo: cards[10].rightAnchor, constant: 5),
-            cards[11].topAnchor.constraint(equalTo: cards[4].bottomAnchor, constant: 5),
-            
-            cards[12].leftAnchor.constraint(equalTo: view.layoutMarginsGuide.leftAnchor, constant: 20),
-            cards[12].topAnchor.constraint(equalTo: cards[8].bottomAnchor, constant: 5),
-            cards[13].leftAnchor.constraint(equalTo: cards[12].rightAnchor, constant: 5),
-            cards[13].topAnchor.constraint(equalTo: cards[8].bottomAnchor, constant: 5),
-            cards[14].leftAnchor.constraint(equalTo: cards[13].rightAnchor, constant: 5),
-            cards[14].topAnchor.constraint(equalTo: cards[8].bottomAnchor, constant: 5),
-            cards[15].leftAnchor.constraint(equalTo: cards[14].rightAnchor, constant: 5),
-            cards[15].topAnchor.constraint(equalTo: cards[8].bottomAnchor, constant: 5),
-            
-        ])
+        settingCard()
+        addViews()
+        drawingView()
     }
     
     @objc func touchUpCard(_ sender: UIButton) {
@@ -142,6 +71,111 @@ class ViewController: UIViewController {
             emoji[card.identifier] = cardEmoji.remove(at: randomIndex)
         }
         return emoji[card.identifier] ?? "?"
+    }
+    
+    // MARK: - Drawing View
+    
+    func settingCard() {
+        for _ in 1...16 {
+            let card = UIButton()
+            card.cardSetting(title: "", backgroundColor: .orange, view: view)
+            cards.append(card)
+        }
+    }
+    
+    func addViews() {
+        for card in cards {
+            view.addSubview(card)
+            card.addTarget(self, action: #selector(touchUpCard), for: .touchUpInside)
+        }
+        view.addSubview(flipCountLabel)
+        view.addSubview(newGameButton)
+        view.addSubview(firstStackView)
+        view.addSubview(secondStackView)
+        view.addSubview(thirdStackView)
+        view.addSubview(sumStackView)
+        
+        // newGameButton Action
+        newGameButton.addTarget(self, action: #selector(touchUpNewGame), for: .touchUpInside)
+    }
+    
+    func drawingView() {
+        view.backgroundColor = .black
+        flipCountLabel.text = "Flips: 0"
+        flipCountLabel.textColor = .white
+        flipCountLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        newGameButton.setTitle("New Game", for: .normal)
+        newGameButton.setTitleColor(.white, for: .normal)
+        newGameButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        firstStackView.translatesAutoresizingMaskIntoConstraints = false
+        firstStackView.axis = .horizontal
+        firstStackView.alignment = .center
+        firstStackView.distribution = .fillEqually
+        firstStackView.spacing = 8
+        firstStackView.addArrangedSubview(cards[0])
+        firstStackView.addArrangedSubview(cards[1])
+        firstStackView.addArrangedSubview(cards[2])
+        firstStackView.addArrangedSubview(cards[3])
+        
+        secondStackView.translatesAutoresizingMaskIntoConstraints = false
+        secondStackView.axis = .horizontal
+        secondStackView.alignment = .center
+        secondStackView.distribution = .fillEqually
+        secondStackView.spacing = 8
+        secondStackView.addArrangedSubview(cards[4])
+        secondStackView.addArrangedSubview(cards[5])
+        secondStackView.addArrangedSubview(cards[6])
+        secondStackView.addArrangedSubview(cards[7])
+        
+        thirdStackView.translatesAutoresizingMaskIntoConstraints = false
+        thirdStackView.axis = .horizontal
+        thirdStackView.alignment = .center
+        thirdStackView.distribution = .fillEqually
+        thirdStackView.spacing = 8
+        thirdStackView.addArrangedSubview(cards[8])
+        thirdStackView.addArrangedSubview(cards[9])
+        thirdStackView.addArrangedSubview(cards[10])
+        thirdStackView.addArrangedSubview(cards[11])
+        
+        lastStackView.translatesAutoresizingMaskIntoConstraints = false
+        lastStackView.axis = .horizontal
+        lastStackView.alignment = .center
+        lastStackView.distribution = .fillEqually
+        lastStackView.spacing = 8
+        lastStackView.addArrangedSubview(cards[12])
+        lastStackView.addArrangedSubview(cards[13])
+        lastStackView.addArrangedSubview(cards[14])
+        lastStackView.addArrangedSubview(cards[15])
+        
+        sumStackView.translatesAutoresizingMaskIntoConstraints = false
+        sumStackView.axis = .vertical
+        sumStackView.alignment = .center
+        sumStackView.distribution = .fillEqually
+        sumStackView.spacing = 8
+        sumStackView.addArrangedSubview(firstStackView)
+        sumStackView.addArrangedSubview(secondStackView)
+        sumStackView.addArrangedSubview(thirdStackView)
+        sumStackView.addArrangedSubview(lastStackView)
+        
+        NSLayoutConstraint.activate([
+            flipCountLabel.leftAnchor.constraint(equalTo: view.layoutMarginsGuide.leftAnchor, constant: 5),
+            flipCountLabel.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: 5),
+            flipCountLabel.heightAnchor.constraint(equalToConstant: 50),
+            flipCountLabel.widthAnchor.constraint(equalToConstant: 100),
+            
+            newGameButton.rightAnchor.constraint(equalTo: view.layoutMarginsGuide.rightAnchor, constant: 5),
+            newGameButton.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: 5),
+            newGameButton.heightAnchor.constraint(equalTo: flipCountLabel.heightAnchor),
+            newGameButton.widthAnchor.constraint(equalTo: flipCountLabel.widthAnchor),
+            
+            sumStackView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor, constant: 5),
+            sumStackView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: 5),
+            sumStackView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 5),
+            sumStackView.bottomAnchor.constraint(equalTo: flipCountLabel.topAnchor, constant: 5)
+            
+        ])
     }
 }
 
